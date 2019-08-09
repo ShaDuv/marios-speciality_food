eafcf5fc3c6516c282df5b181f0fe042da50ed74
Shawna Duvall <shawnaduvall@gmail.com@>
2019-08-09 08:52:32 -0700

    start the code for seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+Review.destroy_all
+# found this solution on stack overflow: https://stackoverflow.com/questions/31478855/seeding-associations-in-rails/31480226#31480226
+20.times do
+  name = Faker::Dessert.variety
+  cost = 
+  division = Division.create!(name: name)
+  5.times do
+    first_name =  Faker::Name.first_name
+    last_name =  Faker::Name.last_name
+    employee = Employee.new(first_name: first_name, last_name: last_name)
+    employee.division = division
+    employee.save!
+  end
+end
+
+p "Created #{Division.count} divisions"
+p "Created #{Employee.count} employees"

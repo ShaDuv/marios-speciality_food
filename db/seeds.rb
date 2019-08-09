459a161f8fc349cef9dbeea587680380bf304c12
Shawna Duvall <shawnaduvall@gmail.com@>
2019-08-09 16:51:35 -0700

    adds ability to edit review and adds a few more seeds to the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 Product.destroy_all
 Review.destroy_all
 # found this solution on stack overflow: https://stackoverflow.com/questions/31478855/seeding-associations-in-rails/31480226#31480226
-20.times do
+50.times do
   name = Faker::Dessert.variety
   cost = rand(1.5...27.0).round(2)
   country = Faker::Address.country
   product = Product.create!(name: name, cost: cost, country: country)
-  rand(3..8).times do
+  rand(5...10).times do
     author =  Faker::Name.name
     content_body = Faker::Restaurant.review
     rating = rand(1...5)

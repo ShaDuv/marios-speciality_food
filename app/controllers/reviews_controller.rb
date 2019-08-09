09e45b3563bcc2c7e7745a66e17ee2e6438267c8
Shawna Duvall <shawnaduvall@gmail.com@>
2019-08-09 16:35:33 -0700

    add the controller for the reviews and the new erb file for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,45 @@
 class ReviewsController < ApplicationController
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+    render :new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @product = Product.find(params[:product_id])
+    @review = Employee.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Employee.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @review = Employee.find(params[:id])
+    if @review.update(review_params)
+      redirect_to product_path(@review.product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Employee.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@review.product)
+  end
+
 end

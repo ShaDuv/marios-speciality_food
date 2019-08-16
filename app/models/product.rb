83ca0a109abad21208fe356ee666ff8480516cb5
Shawna Duvall <shawnaduvall@gmail.com@>
2019-08-16 10:39:06 -0700

    set up and passes validation testing for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   before_save(:titleize_product)
+  validates :name, presence: true
+  validates :cost, presence: true
+  validates :country, presence: true
 
     private
       def titleize_product
         self.name = self.name.titleize
       end
-  end
+end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 
   describe Product do
   it { should have_many(:reviews) }
-
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :cost }
+  it { should validate_presence_of :country }
+  
   it("titleizes the name of an product") do
     product = Product.create({name: "souper cake", cost: 2.22, country: "Mexico"})
     expect(product.name()).to(eq("Souper Cake"))

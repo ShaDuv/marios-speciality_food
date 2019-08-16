21d21eb8c0e64425c748260ee51023acf6fb5b36
Shawna Duvall <shawnaduvall@gmail.com@>
2019-08-16 14:18:05 -0700

    gets validation for review lenght working

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,8 @@ class Review < ApplicationRecord
   validates :content_body, presence: true
   validates :rating, presence: true
   validates :rating, inclusion: { in: (1..5),
-    message: "%{value} is not a valid rating" }
+    message: "%{value} is not a valid rating, please rate from 1 to 5." }
+  validates :content_body, length: { minimum: 50 }
+  validates :content_body, length: { maximum: 250 }
+
 end

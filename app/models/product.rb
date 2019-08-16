23f1a8a1c77434b09a0a6e5f7d9cbcdd8e76d910
Shawna Duvall <shawnaduvall@gmail.com@>
2019-08-16 15:16:22 -0700

    scopes not showing

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,9 @@ class Product < ApplicationRecord
   validates :cost, presence: true
   validates :country, presence: true
 
+  scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
+  
+
     private
       def titleize_product
         self.name = self.name.titleize
